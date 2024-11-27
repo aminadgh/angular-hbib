1b4806a54bc07ce776f47bf30940c42043053372
aminadgh <amina.daghari@esprit.tn>
2024-11-27 23:48:32 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,34 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Importez FormsModule
 
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { FormsModule } from '@angular/forms';
 import { ResidenceComponent } from './Residences/residence/residence.component';
+import { ResidenceDetailsComponent } from './Residences/residence-details/residence-details.component';
+import { AddResidenceComponent } from './Residences/add-residence/add-residence.component';
+import { HeaderComponent } from './header/header.component'; // Chemin vers HeaderComponent
+
 
 @NgModule({
   declarations: [
-    AppComponent,
     HeaderComponent,
+    AppComponent,
+    
+    NotFoundComponent,
     HomeComponent,
-    FooterComponent,
-    ResidenceComponent
+    ResidenceComponent,
+    ResidenceDetailsComponent,
+    AddResidenceComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule 
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
